f987946341e53a3dd519b61114035f756b0c431b
Jobanny Zúñiga <jobannyz@gmail.com>
2021-03-18 18:53:05 +0000

    Guardar cambios

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import { ButtonGroup, Button, DropdownButton, Dropdown } from "react-bootstrap";
 import { Context } from "../store/appContext";
 import "../../styles/index.scss";
 
@@ -22,16 +22,23 @@ export const Navbar = () => {
 						</a>
 					</Link>
 				</div>
-				<DropdownButton id="dropdown-basic-button" title={`Favorites ${store.favlist.length}`}>
-					{store.favlist.map((item, index) => {
-						return (
-							<Dropdown.Item key={index} href="">
-								<a id="button_link">{item}</a>
-								<i className="fas fa-trash-alt" onClick={() => actions.deleteFunction(index)} />
-							</Dropdown.Item>
-						);
-					})}
-				</DropdownButton>
+				<ButtonGroup aria-label="Basic example">
+					<DropdownButton variant="primary" id="dropdown-basic-button" title="Favorites">
+						{store.favlist.length > 0 ? (
+							store.favlist.map((item, index) => {
+								return (
+									<Dropdown.Item key={index} href="">
+										<a id="button_link">{item}</a>
+										<i className="fas fa-trash-alt" onClick={() => actions.deleteFunction(index)} />
+									</Dropdown.Item>
+								);
+							})
+						) : (
+							<Dropdown.Item>Empty</Dropdown.Item>
+						)}
+					</DropdownButton>
+					<Button variant="secondary">{store.favlist.length}</Button>
+				</ButtonGroup>
 			</div>
 		</nav>
 	);
